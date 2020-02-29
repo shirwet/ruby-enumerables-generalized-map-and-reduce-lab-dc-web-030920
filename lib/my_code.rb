990016f388631c620bb1cced69af5a586f115ed1
shirwet Elfeel <eshirwet@yahoo.com>
2020-02-29 20:47:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
 # Your Code Here
+def map(array)
+   new= []
+   i = 0
+   while i < array.length
+     new.push(yield(array[i]))
+     
+     i += 1 
+   end 
+   new
+ end
+
+
+def reduce (array, n = nil)
+  i = 0
+total = n 
+
+while i < array.length do 
+total = (yield (array)[i], total)
+
+i+=  
+end
+total
+end 
